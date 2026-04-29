fcf0f332e42b612868f61f8d04f566f48b706de8
Bekalu Assamnew <bassamnew@gmail.com>
2026-04-29 15:23:40 +0300

    Fix training organizer seeder for project schema

--- database/seeders/TrainingOrganizersTableSeeder.php
+++ database/seeders/TrainingOrganizersTableSeeder.php
@@ -2,17 +2,64 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProjectSubawardee;
 use App\Models\TrainingOrganizer;
 use Illuminate\Database\Seeder;
 
 class TrainingOrganizersTableSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
-        for ($i = 1; $i <= 10; $i++) {
-            TrainingOrganizer::create([
-                'training_organizer_name' => "Training Organizer $i"
-            ]);
+        $projects = [
+            [
+                'project_code' => 'IHSS-001',
+                'project_name' => 'iHSS',
+                'subawardees' => ['Mamela', 'Consortium Partner A'],
+            ],
+            [
+                'project_code' => 'RMNCH-002',
+                'project_name' => 'RMNCH Strengthening',
+                'subawardees' => ['Regional Implementer North'],
+            ],
+            [
+                'project_code' => 'YLDP-003',
+                'project_name' => 'Youth Leadership Development',
+                'subawardees' => ['Youth Network Ethiopia'],
+            ],
+            [
+                'project_code' => 'PHC-004',
+                'project_name' => 'Primary Health Care Support',
+                'subawardees' => [],
+            ],
+            [
+                'project_code' => 'MNH-005',
+                'project_name' => 'Maternal and Newborn Health Initiative',
+                'subawardees' => ['Safe Birth Alliance'],
+            ],
+        ];
+
+        foreach ($projects as $definition) {
+            $organizer = TrainingOrganizer::query()->updateOrCreate(
+                ['project_code' => $definition['project_code']],
+                [
+                    'project_code' => $definition['project_code'],
+                    'project_name' => $definition['project_name'],
+                    'title' => $definition['project_name'],
+                ]
+            );
+
+            foreach ($definition['subawardees'] as $index => $name) {
+                ProjectSubawardee::query()->updateOrCreate(
+                    [
+                        'project_id' => $organizer->id,
+                        'subawardee_name' => $name,
+                    ],
+                    [
+                        'project_id' => $organizer->id,
+                        'subawardee_name' => $name,
+                    ]
+                );
+            }
         }
     }
 }
